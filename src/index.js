fdd919d08fe4ce269527350e1bfadd1db1627652
Eric Vennemeyer <henri.barun@gmail.com>
2022-09-12 11:43:07 -0700

    App complete.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const app = (
-  <div className="app-container">
-    <h1>Sample Title</h1>
-    <p>Sample Text</p>
-  </div>
+  <App />
 )
 
 root.render(app);
