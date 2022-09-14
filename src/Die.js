1aaeb674c5d91feb020928c45b955a26bca295f1
Eric Vennemeyer <henri.barun@gmail.com>
2022-09-14 11:31:18 -0700

    Change dice to display pips instead of numbers.

--- src/Die.js
+++ src/Die.js
@@ -3,9 +3,14 @@ import React from "react";
 export default function Die(props) {
     const classNames = `die ${props.isHeld ? "held" : ""}`;
 
+    const dotsArray = [];
+    for (let i = 0; i < props.number; i++) {
+        dotsArray.push(<span className="dot"></span>);
+    }
+
     return (
         <div className={classNames} onClick={props.handleClick}>
-            {props.number}
+            {dotsArray}
         </div>
     )
 }
